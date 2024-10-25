ebb26aaca8eba81926189ac89214ff109cf21258
Hugo <hugogarciarubio@gmail.com>
2024-10-25 16:51:29 +0200

    pr5

--- pr5/app/src/main/java/com/example/pr5/MainActivity.java
+++ pr5/app/src/main/java/com/example/pr5/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.pr5;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button botonContinuar;
+    private TextView textoOculto;
+    private EditText cajaCorreo;
+    private EditText cajaTextoPass;
+    private Switch switch2;
+    private TextView textoOculto2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +32,37 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        botonContinuar = findViewById(R.id.switch1);
+        textoOculto = findViewById(R.id.textView);
+        cajaCorreo = findViewById(R.id.cajaTextoCorreo);
+        cajaTextoPass = findViewById(R.id.cajaTextoPass);
+        switch2 = findViewById(R.id.switch1);
+        textoOculto2 = findViewById(R.id.textView2);
 
+        botonContinuar.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.button){
+            if (cajaCorreo.getText().toString().equals("correo@correo.com") && cajaTextoPass.getText().toString().equals("1234")){
+                textoOculto.setVisibility(View.VISIBLE);
+                textoOculto.setTextColor(getColor(R.color.green));
+                textoOculto.setText("Usuario y contraseña correcto.");
+                if(switch2.isChecked()){
+                    textoOculto2.setVisibility(View.VISIBLE);
+                    textoOculto2.setTextColor(getColor(R.color.green));
+                    textoOculto2.setText("Información de acceso almacenada correctamente.");
+                }
+            } else {
+                textoOculto.setVisibility(View.VISIBLE);
+                textoOculto.setTextColor(getColor(R.color.red));
+                textoOculto.setText("Usuario y/o contraseña incorrectos.");
+            }
+
+        }
 
     }
 }
